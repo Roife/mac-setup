cd49b0be4f571518b918f0bcd8f8fa520b0302b7
roife <roifewu@gmail.com>
2017-07-30 13:17:42 +0800

    rename aria2.conf

--- rc.sh
+++ rc.sh
@@ -5,4 +5,4 @@ cp ./.wgetrc ~/.wgetrc
 cp ./.clang-format ~/.clang-format
 cp ./.gitconfig ~/.gitconfig
 cp ./.gitignore ~/.gitignore
-cp ./aria2.config ~/.aria2/aria2.config
+cp ./aria2.conf ~/.aria2/aria2.config
