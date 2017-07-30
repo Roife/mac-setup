e5330944ed3d1546cf0a454f484f3097fe61572c
roife <roifewu@gmail.com>
2017-07-30 13:27:19 +0800

    auto download Vundle & auto install plugins

--- rc.sh
+++ rc.sh
@@ -1,6 +1,10 @@
 cp ./.zshrc ~/.zshrc
 cp ./.tmux.conf ~/.tmux.conf
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ./.vimrc ~/.vimrc
+vim +PluginInstall +qall
+
 cp ./.wgetrc ~/.wgetrc
 cp ./.clang-format ~/.clang-format
 cp ./.gitconfig ~/.gitconfig
